4eadbbb27325758b0893bdd90ac9f391b41144e0
MarvinQuiet <mawenjing1993@126.com>
2019-02-07 11:40:52 -0500

    edit the core cluster would use

--- marge_bart.py
+++ marge_bart.py
@@ -227,8 +227,7 @@ def do_marge_bart(user_data):
     with open(slurm_file, 'w') as fopen:
         fopen.write('''#!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 4
-#SBATCH --mem=100000
+#SBATCH -n 12
 #SBATCH -t 24:00:00
 #SBATCH -p standard
 #SBATCH -A zanglab
